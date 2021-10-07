7586157a8a1c7f7bd2c93e80e9f5e943462e8ebc
62fe50 <merrickslane@gmail.com>
2021-10-07 00:17:32 -0700

    add 3D support

--- main.cpp
+++ main.cpp
@@ -2,12 +2,58 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "shader.h"
 #include "image.h"
 
-static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+float posx = 0;
+float posy = 0;
+float posz = 0;
+
+float viewx = 0;
+float viewy = 0;
+
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {	
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+
+	if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+		switch (key) {
+			case GLFW_KEY_W:
+				posx += 0.1f;
+				break;
+			case GLFW_KEY_S:
+				posx -= 0.1f;
+				break;
+			case GLFW_KEY_A:
+				posz -= 0.1f;
+				break;
+			case GLFW_KEY_D:
+				posz += 0.1f;
+				break;
+			case GLFW_KEY_R:
+				posy -= 0.1f;
+				break;
+			case GLFW_KEY_F:
+				posy += 0.1f;
+				break;
+			case GLFW_KEY_UP:
+				viewy += 0.1f;
+				break;
+			case GLFW_KEY_DOWN:
+				viewy -= 0.1f;
+				break;
+			case GLFW_KEY_LEFT:
+				viewx += 0.1f;
+				break;
+			case GLFW_KEY_RIGHT:
+				viewx -= 0.1f;
+				break;
+		}
 	}
 }
 
@@ -27,6 +73,7 @@ int main(void) {
 	}
 
 	glfwSetKeyCallback(window, keyCallback);
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
 	glfwSetFramebufferSizeCallback(window, sizeCallback);
 
 	glfwMakeContextCurrent(window);
@@ -44,11 +91,57 @@ int main(void) {
 	glDeleteShader(fShader);
 
 	/* temporoary triangle stuff */
+	/*
 	float vertices[] = {
 	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 5.0f, 0.0f,
-	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 2.5f, 2.5f
+	 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 10.0f, 0.0f,
+	 0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 5.0f, 5.0f
 	};
+	*/
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
 
 	GLuint VBO;
 	glGenBuffers(1, &VBO);
@@ -58,27 +151,69 @@ int main(void) {
 	GLuint VAO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), NULL); //position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), NULL); //position
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); //color
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float))); //texture
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); //texture
-	glEnableVertexAttribArray(2);
 
-	GLuint texture = createTextureFromBMP("amogus.bmp", GL_MIRRORED_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+	GLuint texture = createTextureFromBMP("obama.bmp", GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+
+	glEnable(GL_DEPTH_TEST);
 
 	/* main loop */
 	while (!glfwWindowShouldClose(window)) {
+		int width, height;
+		glfwGetWindowSize(window, &width, &height);
+
 		/* draw background color */
 		glClearColor(0.0f, 0.2f, 0.5f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glm::vec3 cameraDirection(
+			cos(viewy) * sin(viewx),
+			sin(viewy),
+			cos(viewx) * cos(viewy)
+		);
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		glm::vec3 pos = glm::vec3(posx, posy, posz);
+
+		model = glm::rotate(model, 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(posx, posy, posz));
+		view = glm::lookAt(pos, pos + cameraDirection, glm::vec3(0.0f, 1.0f, 0.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+		unsigned int modelL = glGetUniformLocation(shaderProgram, "model");
+		unsigned int viewL = glGetUniformLocation(shaderProgram, "view");
+
+		glUniformMatrix4fv(modelL, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewL, 1, GL_FALSE, &view[0][0]);
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "proj"), 1, GL_FALSE, &proj[0][0]);
+
+		for (unsigned int i = 0; i < 10; i++) {
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3((float)i, (float)i, -1 * (float)i));
+			model = glm::rotate(model, (float)glfwGetTime() * 3, glm::vec3(0.0f, 1.0f, 0.0f));
+
+			unsigned int modelL = glGetUniformLocation(shaderProgram, "model");
+			unsigned int viewL = glGetUniformLocation(shaderProgram, "view");
+
+			glUniformMatrix4fv(modelL, 1, GL_FALSE, glm::value_ptr(model));
+			glUniformMatrix4fv(viewL, 1, GL_FALSE, &view[0][0]);
+			glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "proj"), 1, GL_FALSE, &proj[0][0]);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		/* draw triangle */
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		/* this code should go last */
 		glfwPollEvents();
